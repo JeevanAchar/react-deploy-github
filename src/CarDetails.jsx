9c816495eb1804fa2b9ac64c901f3fca94070173
JeevanAchar <Jeevanachar83@gmail.com>
2023-03-09 20:12:26 +0530

    JSON

--- src/CarDetails.jsx
+++ src/CarDetails.jsx
@@ -9,8 +9,7 @@ const CarDetails = (props) => {
         <h3>{props.details[0].carColor}</h3>
         <h3>{props.details[0].carCompany}</h3>
         <h4>{props.details[0].carType}</h4>
-        <img src={props.details[0].carImage} alt="" />
-        {/* <h4>{props.details[0].carComponents}</h4> */}
+        <img src={props.details[0].carImage} alt="img"/>
         </div>
     </div>
   )
